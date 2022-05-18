00e071f48b53b4621c0d3193edbac13a244a5ec6
Maria <maria.senina15@gmail.com>
2022-05-18 01:25:55 -0400

    Set up a module

---
+++ modules/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2module" {
+  source = "./ec2"
+  ec2name = "Name From Module" # <- assign a value to the variable we declared in the ec2 module
+}
+
+# NOTE: run ```terraform init``` every time you make a change to the modules, so Terraform is aware of which modules are in use
