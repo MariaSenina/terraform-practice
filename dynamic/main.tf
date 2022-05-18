ad51e7a09050edc6c633ad5517537f0e48436c1a
Maria <maria.senina15@gmail.com>
2022-05-17 23:47:30 -0400

    Using dynamic blocks

---
+++ dynamic/main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "ingressrules" {
+  type = list(number)
+  default = [ 80, 443 ]
+}
+
+variable "egressrules" {
+  type = list(number)
+  default = [ 80, 443, 25, 3306, 53, 8080 ]
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-0022f774911c1d690"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.webtraffic.name] # <- link the security group from below to our EC2 instance
+}
+
+resource "aws_security_group" "webtraffic" {
+  name = "Allows HTTPS"
+
+# allow the traffic in
+  dynamic "ingress" {
+    iterator = port # <- this will represent each element in our list
+    for_each = var.ingressrules
+    content {
+      from_port = port.value
+      to_port = port.value
+      protocol = "TCP"
+      cidr_blocks = ["0.0.0.0/0"] # <- allow to all traffic
+    }
+  }
+
+# allows the traffic out
+  dynamic "egress" {
+    iterator = port # <- this will represent each element in our list
+    for_each = var.egressrules
+    content {
+      from_port = port.value
+      to_port = port.value
+      protocol = "TCP"
+      cidr_blocks = ["0.0.0.0/0"] # <- allow to all traffic
+    }
+  }
+}
